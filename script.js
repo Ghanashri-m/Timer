ccebc45b973b0188f5fefd890be6da48cab54c27
Ghanashri-m <ghanashri.28@gmail.com>
2021-03-06 08:09:03 +0530

    Practice task timer

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(
+    function(){
+        document.getElementById('demo').innerHTML = 10;
+        setTimeout(
+            function(){
+                document.getElementById('demo').innerHTML = 9;
+                setTimeout(
+                    function(){
+                        document.getElementById('demo').innerHTML = 8;
+                        setTimeout(
+                            function(){
+                                document.getElementById('demo').innerHTML = 7;
+                                setTimeout(
+                                    function(){
+                                        document.getElementById('demo').innerHTML = 6;
+                                        setTimeout(
+                                            function(){
+                                                document.getElementById('demo').innerHTML = 5;
+                                                setTimeout(
+                                                    function(){
+                                                        document.getElementById('demo').innerHTML = 4;
+                                                        setTimeout(
+                                                            function(){
+                                                                document.getElementById('demo').innerHTML = 3;
+                                                                setTimeout(
+                                                                    function(){
+                                                                        document.getElementById('demo').innerHTML = 2;
+                                                                        setTimeout(
+                                                                            function(){
+                                                                                document.getElementById('demo').innerHTML = 1;
+                                                                                setTimeout(
+                                                                                    function(){
+                                                                                        document.getElementById('demo').innerHTML = 'Happy Independence Day';
+                                                                                    }, 1000);
+                                                                            }, 1000);
+                                                                    }, 1000);
+                                                            }, 1000);
+                                                    }, 1000);
+                                            }, 1000);
+                                    }, 1000);
+                            }, 1000);
+                    }, 1000);
+            }, 1000);
+    }, 1000);
